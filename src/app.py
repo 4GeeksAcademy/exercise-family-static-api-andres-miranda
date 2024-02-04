9f8ecc2909f9b9fc4a883cc2f0716f8ae5f7ebaa
Andrés Miranda <31228376+andresmiranda2000@users.noreply.github.com>
2024-02-04 16:30:11 +0000

    finalizado

--- src/app.py
+++ src/app.py
@@ -2,25 +2,21 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -46,7 +42,7 @@ def create_member():
     }
     jackson_family.add_member(new_member)
     response_body = {
-        "msg": "New member successfully added",
+        "msg": "Miembro Agregado",
         "member":  new_member
     }
     return jsonify(response_body), 200
